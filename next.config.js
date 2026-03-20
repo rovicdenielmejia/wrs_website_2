8491c11ac36a56991d7b95b699ee948c01fe50e7
Rovic <rovicdenielmejia@gmail.com>
2026-03-20 17:14:57 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -11,6 +11,14 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config, { isServer }) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'react': require.resolve('react'),
+      'react-dom': require.resolve('react-dom'),
+    };
+    return config;
+  },
   async headers() {
     return [
       {
