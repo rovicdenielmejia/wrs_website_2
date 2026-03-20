43171a6bdf16de81cf35a981aa60d3a3d9a47842
Rovic <rovicdenielmejia@gmail.com>
2026-03-20 17:09:12 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ['@radix-ui/react-slot', '@radix-ui/react-compose-refs'],
   images: {
     domains: ['localhost', 'workforcerecruitmentsolutions.com'],
     remotePatterns: [
